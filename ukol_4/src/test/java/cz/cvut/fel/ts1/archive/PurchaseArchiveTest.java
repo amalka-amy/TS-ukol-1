460c9088ab78c26d85825ec10b614b57f1e21ac9
Amálie Jirotková <amy.jirotkova@gmail.com>
2024-04-12 12:27:10 +0200

    PurchaseArchiveTest.java Done

---
+++ ukol_4/src/test/java/cz/cvut/fel/ts1/archive/PurchaseArchiveTest.java
@@ -0,0 +1,92 @@
+package cz.cvut.fel.ts1.archive;
+
+import cz.cvut.fel.ts1.shop.Item;
+import cz.cvut.fel.ts1.shop.Order;
+import cz.cvut.fel.ts1.shop.ShoppingCart;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+public class PurchaseArchiveTest {
+
+    int id = 10;
+    String name = "TestItem";
+    float price = 100;
+    String category = "TestCategory";
+
+
+    private final HashMap<Integer, ItemPurchaseArchiveEntry> itemPurchaseArchive = new HashMap<>();
+    private ArrayList<Order> orderArchive;
+    PurchasesArchive archiveShort = new PurchasesArchive();
+    PurchasesArchive archiveLong = new PurchasesArchive(itemPurchaseArchive, orderArchive);
+
+    @Test
+    public void testPrintItemPurchaseStatisticsLong(){
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        StringBuilder expectedOutput = new StringBuilder("ITEM PURCHASE STATISTICS:\n");
+        for(ItemPurchaseArchiveEntry e : itemPurchaseArchive.values()) {
+            expectedOutput.append(e.toString()).append("\n");
+        }
+        archiveLong.printItemPurchaseStatistics();
+        assertEquals(expectedOutput.toString(), outContent.toString());
+        System.setOut(System.out);
+    }
+    @Test
+    public void testPrintItemPurchaseStatisticsShort(){
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        StringBuilder expectedOutput = new StringBuilder("ITEM PURCHASE STATISTICS:\n");
+        for(ItemPurchaseArchiveEntry e : itemPurchaseArchive.values()) {
+            expectedOutput.append(e.toString()).append("\n");
+        }
+        archiveShort.printItemPurchaseStatistics();
+        assertEquals(expectedOutput.toString(), outContent.toString());
+        System.setOut(System.out);
+    }
+    @Test
+    public void testGetHowManyTimesHasBeenItemSold(){
+        Item itemTest = new Item(id, name,price, category);
+        if(itemPurchaseArchive.containsKey(itemTest.getID())){
+            int expected = itemPurchaseArchive.get(itemTest.getID()).getCountHowManyTimesHasBeenSold();
+            assertEquals(expected, archiveShort.getHowManyTimesHasBeenItemSold(itemTest));
+            assertEquals(expected, archiveLong.getHowManyTimesHasBeenItemSold(itemTest));
+        }
+        assertEquals(0,archiveShort.getHowManyTimesHasBeenItemSold(itemTest));
+        assertEquals(0,archiveLong.getHowManyTimesHasBeenItemSold(itemTest));
+    }
+
+    @Test
+    public void testPutOrderToPurchasesArchive_lengthOfArchive(){
+        Order order = new Order(new ShoppingCart(), "Pepa", "Praha");
+        ArrayList <Order> expected = archiveShort.getOrderArchive();
+        expected.add(order);
+        archiveShort.putOrderToPurchasesArchive(order);
+        assertEquals(expected,archiveShort.getOrderArchive());
+
+    }
+    @Test
+    public void testPutOrderToPurchasesArchive_items(){
+        Order order = new Order(new ShoppingCart(), "Pepa", "Praha");
+        ArrayList <Order> expected = archiveShort.getOrderArchive();
+        ArrayList<Item> items = order.getItems();
+        for(Item i : items){
+            if(itemPurchaseArchive.containsKey(i.getID())){
+                ItemPurchaseArchiveEntry e = itemPurchaseArchive.get(i.getID());
+                e.increaseCountHowManyTimesHasBeenSold(1);
+            }else{
+                itemPurchaseArchive.put(i.getID(), new ItemPurchaseArchiveEntry(i));
+            }
+        }
+
+        archiveShort.putOrderToPurchasesArchive(order);
+        assertEquals(expected,archiveShort.getOrderArchive());
+
+    }
+
+}
